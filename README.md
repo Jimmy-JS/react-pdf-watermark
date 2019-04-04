ee0bd41e961c6d8987211a61a6461027fe57b286
Jimmy Setiawan <jimmysetiawan.js@gmail.com>
2019-04-04 18:22:19 +0700

    init

---
+++ README.md
@@ -0,0 +1,110 @@
+# react-pdf-js
+
+`react-pdf-viewer` provides a component for rendering PDF documents using `mozilla/pdf.js` and ability to adding watermark to PDF. Written for React 15/16 and ES2015.
+
+This package is originally built by `mikecousins/react-pdf-js` with a number of enhancement:
+* Added hook `onPageRenderComplete()` for determine whether the page is loaded onCanvas.
+* Watermark - For displaying watermark on top of original PDF.
+* Add check handler for duplicated canvas load.
+* Add custom pdf.worker file
+
+## Usage
+
+Install with `npm install react-pdf-viewer`
+
+## Example
+
+Use it in your app (showing some basic pagination as well):
+
+```js
+import React from 'react';
+import PDF from 'react-pdf-viewer';
+
+class MyPdfViewer extends React.Component {
+  state = {};
+
+  handlePrevious = () => {
+    this.setState({ page: this.state.page - 1 });
+  }
+
+  handleNext = () => {
+    this.setState({ page: this.state.page + 1 });
+  }
+
+  renderPagination = (page, pages) => {
+    let previousButton = <li className="previous" onClick={this.handlePrevious}><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
+    if (page === 1) {
+      previousButton = <li className="previous disabled"><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
+    }
+    let nextButton = <li className="next" onClick={this.handleNext}><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
+    if (page === pages) {
+      nextButton = <li className="next disabled"><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
+    }
+    return (
+      <nav>
+        <ul className="pager">
+          {previousButton}
+          {nextButton}
+        </ul>
+      </nav>
+    );
+  }
+
+  render() {
+    let pagination = null;
+    if (this.state.pages) {
+      pagination = this.renderPagination(this.state.page, this.state.pages);
+    }
+    return (
+      <div>
+        <PDF
+          file="test.pdf"
+          page={this.state.page}
+          watermark="WATERMARK GOES HERE.."
+          watermarkOptions={{
+            transparency: 0.5,
+            fontSize: 55,
+            fontStyle: 'Bold',
+            fontFamily: 'Arial'
+          }}
+          onDocumentComplete={ () => { /* Do anything on document loaded like remove loading, etc */ }}
+          onPageRenderComplete={(pages, page) => this.setState({ page, pages })}
+        />
+        {pagination}
+      </div>
+    )
+  }
+}
+
+export default MyPdfViewer;
+```
+
+## Custom Watermark Styling
+
+For more watermark styling, you can customize it yourself by creating watermark render function that will receipt canvas and context as params.
+
+```js
+  ...
+
+  applyWatermark = (canvas, context) => {
+    context.globalAlpha = 0.15
+    context.font = '55px bold Arial'
+    context.translate(canvas.width / 2, canvas.height / 2)
+    context.rotate(-Math.atan(canvas.height / canvas.width)) // Rotate watermark to show diagonally
+
+    const text = 'Strictly Confidential. Not to be circulated'
+    metrics = context.measureText(text)
+    context.fillText(text, -metrics.width / 2, (55 / 2))
+  }
+
+  ...
+```
+
+And then apply it to your PDF component:
+```js
+  <PDF
+    file="test.pdf"
+    ...
+    watermark={this.applyWatermark}
+  />
+```
---
+++ src/index.js
@@ -0,0 +1,148 @@
+import PdfJsLib from 'pdfjs-dist'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Pdf extends Component {
+  static propTypes = {
+    file: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object
+    ]).isRequired,
+    watermark: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.func
+    ]),
+    watermarkOptions: PropTypes.object,
+    page: PropTypes.number,
+    onDocumentComplete: PropTypes.func,
+    onPageRenderComplete: PropTypes.func,
+    onChangePage: PropTypes.func,
+    forceRerender: PropTypes.bool,
+    scale: PropTypes.number,
+    cMapUrl: PropTypes.string,
+    cMapPacked: PropTypes.bool,
+    className: PropTypes.string,
+    workerSrc: PropTypes.string
+  }
+
+  static defaultProps = {
+    page: 1,
+    watermark: null,
+    watermarkOptions: {},
+    onDocumentComplete: null,
+    onPageRenderComplete: null,
+    onChangePage: null,
+    forceRerender: false,
+    scale: 1,
+    cMapUrl: '../node_modules/pdfjs-dist/cmaps/',
+    cMapPacked: false,
+    className: '',
+    workerSrc: '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js'
+  }
+
+  state = {
+    pdf: null,
+    numPages: 0
+  }
+
+  renderTask = null
+
+  componentDidMount() {
+    this.renderPDF()
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { page, scale, forceRerender, onChangePage } = this.props
+    const { pdf } = this.state
+
+    if (
+      ((newProps.page !== page) || (newProps.scale !== scale)) &&
+      pdf
+    ) {
+      pdf.getPage(newProps.page).then((p) => {
+        this.drawPDF(p)
+
+        if (onChangePage) onChangePage(p.pageNumber)
+      })
+    }
+
+    if (newProps.forceRerender && newProps.forceRerender !== forceRerender && pdf) {
+      this.renderPDF()
+    }
+  }
+
+  renderPDF = () => {
+    const { file, page, cMapUrl, cMapPacked, onDocumentComplete, workerSrc } = this.props
+    PdfJsLib.GlobalWorkerOptions.workerSrc = workerSrc
+    PdfJsLib.getDocument({ url: file, cMapUrl, cMapPacked }).then((pdf) => {
+      this.setState({ pdf, numPages: pdf._pdfInfo.numPages })
+
+      if (onDocumentComplete) onDocumentComplete(pdf._pdfInfo.numPages)
+
+      pdf.getPage(page).then(p => this.drawPDF(p))
+    })
+  }
+
+  drawPDF = (page) => {
+    const { scale } = this.props
+    const viewport = page.getViewport(scale)
+    const { canvas } = this
+    const canvasContext = canvas.getContext('2d')
+    canvas.height = viewport.height
+    canvas.width = viewport.width
+    const renderContext = { canvasContext, viewport }
+
+    if (this.renderTask) {
+      this.renderTask.cancel()
+    }
+
+    this.renderPage(page, renderContext)
+  }
+
+  renderPage = (page, renderContext) => {
+    const { onPageRenderComplete, watermark } = this.props
+    const { numPages } = this.state
+    const { canvas } = this
+    const canvasContext = canvas.getContext('2d')
+    this.renderTask = page.render(renderContext)
+    this.renderTask.promise.then(() => {
+      if (onPageRenderComplete) onPageRenderComplete(numPages, page.pageNumber, canvas, canvasContext)
+
+      if (watermark) {
+        if (typeof watermark === 'function') watermark(canvas, canvasContext)
+        else if (typeof watermark === 'string') this.renderBasicWatermark()
+      }
+    }).catch((err) => {
+      this.renderTask = null
+      if (err.name === 'RenderingCancelledException') {
+        this.renderPage(page, renderContext)
+      }
+    })
+  }
+
+  renderBasicWatermark = () => {
+    const { canvas } = this
+    const context = canvas.getContext('2d')
+    const { watermark, watermarkOptions } = this.props
+
+    let options = {
+      transparency: 0.5,
+      fontSize: 55,
+      fontStyle: 'Bold',
+      fontFamily: 'Arial'
+    }
+    options = { ...options, ...watermarkOptions }
+
+    context.globalAlpha = options.transparency
+    context.font = `${options.fontSize}px ${options.fontStyle} ${options.fontFamily}`
+    context.translate(canvas.width / 2, canvas.height / 2)
+
+    const metrics = context.measureText(watermark)
+    context.fillText(watermark, -metrics.width / 2, (55 / 2))
+  }
+
+  render() {
+    const { className } = this.props
+    return <canvas ref={(canvas) => { this.canvas = canvas }} className={className} />
+  }
+}
